b492784032512ac521721581aa242b21ba69e04a
Cachwir <cachwir@gmx.com>
2015-10-21 14:33:57 +0200

    Created the index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="shortcut icon" type="image/x-icon" href="design/images/favicon.ico" />
+    </head>
+
+    <body>
+    	<p>Il est 6h37 du matin et :</p>
+		<ul>
+			<li>J'ai envie de retourner au lit</li>
+			<li>sauf que j'ai un truc à faire</li>
+			<li>alors je mange mon petit dej</li>
+			<li>en faisant un TP</li>
+			<li>tout en regardant l'heure</li>
+			<li>pour ne pas être en retard.</li>
+		</ul>
+
+	</body>
+</html>
